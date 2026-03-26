82841b6ee6e0cdbfe6dee05738ab8f789d118445
Gagan Pandey <millionairegagan123@gmail.com>
2026-03-26 23:57:46 +0530

    we have completed the most of the basic required to learn

---
+++ 02_basics/dom.js
@@ -0,0 +1,32 @@
+/*The DOM (Document Object Model) is a programming interface that represents a web page as a structured tree of objects, allowing developers to access, modify, and interact with its 
+content using languages like JavaScript. It’s the foundation for dynamic web behavior, enabling tasks such as updating text,
+ handling events, and manipulating styles in real time.
+ Definition: The DOM is a tree-like representation of an HTML, XML, or SVG document in memory.
+
+  help to :-
+  dynamic updates : change page content without reloading 
+  form validations : check user input before submission 
+  interactive ui: dropdown menu , modals , sliders ,
+  Event handling : Respond to clicks , key presses , or mouse movements.
+Structure:
+
+Root node: <html>
+
+Child nodes: <head> and <body>
+
+Nested nodes: <div>, <p>, <ul>, <li>, etc.
+
+Nodes: Every element, attribute, and piece of text is represented as a node.
+
+Interface: Provides methods like getElementById(), querySelector(), and properties like innerHTML to manipulate content.
+
+ */
+ 
+// these are the imp topics
+/*Access elements: document.getElementById("demo")
+
+Modify content: element.innerHTML = "Hello World"
+
+Change styles: element.style.color = "red"
+
+Attach events: element.addEventListener("click", handler)*/
